d29d1fb4266054d9fa2d4a69b264ecaa2eb3e4f4
Derek Shea <derek.t.shea@gmail.com>
2020-03-19 15:15:42 -0400

    Add args, cleanup comments.

--- hashtree.sh
+++ hashtree.sh
@@ -5,12 +5,14 @@ function hashtree {
   elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo -e $HELP_STR
   else
-    echo -e $1
-    local OUTPUT="checksums.md5" #TODO: after args
+    # INIT VARS
+    local OUTPUT=""
     local SUM_TYPE="md5"
+    local SUM_FUNC="md5sum"
     local CHECK=false
     local OPTIONS=""
 
+    # GET ARGS
     while [[ $# > 1 ]]; do
       local key="$1"
       case $key in
@@ -18,10 +20,20 @@ function hashtree {
           echo -e $HELP_STR
           break  # exit function if -h used
           ;;
+        --md5|--md5sum)
+          SUM_TYPE="md5"
+          SUM_FUNC="md5sum"
+          ;;
+        --sha256|--sha256sum)
+          SUM_TYPE="sha256"
+          SUM_FUNC="sha256sum"
+          ;;
         -c|--check)
           CHECK=true
           ;;
         -o|--output)
+          # next arg is output filepath
+          # also for non-standard path in sum checks
           OUTPUT=$2
           shift
           ;;
@@ -31,22 +43,29 @@ function hashtree {
       esac
       shift
     done
-    local DIR=$1 
+    #TODO: make absolute
+    local DIR=$1  # head directory for HashTree
 
     if [ "$OUTPUT" = "" ]; then
       OUTPUT="$SUM_TYPE_checksums"
     fi
 
+    # MAIN CHECKSUM CALL
+    # note: option structure for sum functions must be identical to work
     if [ -d "$DIR" ]; then  # if $DIR directory exists
       cd $DIR  # change to $DIR directory
+      # CHECK/COMPARE SUMS
       if [ "$CHECK" = true ]; then  # if -c or --check option specified
         md5sum --check $OPTIONS $OUTPUT  # check MD5 sums in $OUTPUT file
-      else                          # else
-        find . -type f ! -name "$OUTPUT" -exec md5sum $OPTIONS {} + > $OUTPUT  # Calculate MD5 sums for files in current directory and subdirectories excluding $OUTPUT file and save result in $OUTPUT file
+      # HASH FILES & STORE
+      else
+        # calculate hash for files in current dir & subdirs excl. $OUTPUT file
+        # save result in $OUTPUT file
+        find . -type f ! -name "$OUTPUT" -exec md5sum $OPTIONS {} + > $OUTPUT
       fi
       cd - > /dev/null  # change to previous directory
     else
-      cd $DIR  # if $DIR doesn't exists, change to it to generate localized error message
+      cd $DIR  # if $DIR doesn't exists, cd to generate localized error message
     fi
   fi
 }
